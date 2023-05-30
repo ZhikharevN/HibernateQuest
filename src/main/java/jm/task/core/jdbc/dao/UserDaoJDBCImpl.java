1ccf3e6fcede5adf8e5682e9f8f38e95725650e0
Zhikharev <zhikharevxx@mail.ru>
2023-05-30 08:59:21 +0300

    TransactionJBDC

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
 
-    Connection connection = Util.getConnection();
+    private Connection connection = Util.getConnection();
 
     public UserDaoJDBCImpl() {
 
@@ -39,11 +39,14 @@ public class UserDaoJDBCImpl implements UserDao {
     public void saveUser(String name, String lastName, byte age) {
         String sql = "INSERT INTO USERS(NAME, LASTNAME, AGE) VALUES(?, ?, ?)";
         try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            connection.setAutoCommit(false);
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, lastName);
             preparedStatement.setByte(3, age);
             preparedStatement.executeUpdate();
+            connection.commit();
             System.out.printf("User с именем – %s добавлен в базу данных\n", name);
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -54,8 +57,10 @@ public class UserDaoJDBCImpl implements UserDao {
         String sql = "DELETE FROM USERS WHERE ID = (?)";
 
         try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            connection.setAutoCommit(false);
             preparedStatement.setLong(1, id);
             preparedStatement.executeUpdate();
+            connection.commit();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -67,6 +72,7 @@ public class UserDaoJDBCImpl implements UserDao {
 
         String sql = "SELECT * FROM USERS";
         try (Statement statement = connection.createStatement()) {
+            connection.setAutoCommit(false);
             ResultSet resultSet = statement.executeQuery(sql);
             while (resultSet.next()) {
                 User user = new User(resultSet.getString("name"),
@@ -77,6 +83,7 @@ public class UserDaoJDBCImpl implements UserDao {
 
                 list.add(user);
             }
+            connection.commit();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -85,7 +92,6 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void cleanUsersTable() {
         String sql = "DELETE FROM USERS";
-
         try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
